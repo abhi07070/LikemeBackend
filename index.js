f47c39a01796faf3a1285ab77bec51370a10f5f0
Davinder Kumar <abhiverma97819@gmail.com>
2023-06-22 21:49:11 +0530

    add a ejs file

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8000;
 //use express router
 app.use('/', routes);
 
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views')
+
 app.listen(port, () => {
     console.log(`listening at port ${port}`);
 })
